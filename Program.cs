159ca30a897108ba045f58c01a93acecfc1610b9
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-11-29 19:24:53 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace _nstance_field_property
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Söz Dizimi:
+                class SinifAdi
+                {
+                    [Erisim Belirleyici] [Veri Tipi] OzellikAdi;
+                    [Erisim Belirleyici] [Geri Donus Tipi] MetotAdi([Parametre Listesi])
+                    {
+                        METOT KOMUTLARI
+                    }
+                }
+
+                Erisim Belirleyicileri:
+                * Public
+                * Private
+                * Internal
+                * Protected
+            */
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayse";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 2154513;
+            calisan1.Departman = "Insan Kaynaklari";
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("*********");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 5546546;
+            calisan2.Departman = "Satin Alma";
+            calisan2.CalisanBilgileri();
+        }
+
+        class Calisan
+        {
+            public string Ad;
+            public string Soyad;
+            public int No;
+            public string Departman;
+            public void CalisanBilgileri()
+            {
+                Console.WriteLine("Calisan Adi: {0}", Ad);
+                Console.WriteLine("Calisan Soyad: {0}", Soyad);
+                Console.WriteLine("Calisan No: {0}", No);
+                Console.WriteLine("Calisan Departmani: {0}", Departman);
+            }
+        }
+    }
+}
